98193fc58b2ee24193f9cdedc6b8be080d6217ea
Nicolas Rossello <131174762+number1angel@users.noreply.github.com>
2023-08-28 14:34:41 -0300

    updateSaldo

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -1,24 +1,29 @@
 <?php
 namespace TrabajoSube;
 class Tarjeta{
-    public $saldo;
+    private $saldo;
+    
     private array $cargasPermitidas = array (150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1100, 1200, 13000, 1400, 1500, 2000, 2500, 3000, 3500, 4000);
     public function __construct($saldo = 0) {
         $this->saldo = $saldo;
     }
+    
     public function verSaldo(){
         return $this->saldo;
-        }
-    public function cargarSaldo($carga = 0){
+    }
+    public function updateSaldo($carga){
+        $this->saldo += $carga;
+    }
+    public function cargarSaldo(){
         if (in_array($carga, $this->cargasPermitidas) {
             if($carga + $saldo <= 6600){
-                $this->saldo += $carga;
+                $this->updateSaldo($carga);
             }
             else{
-            echo "Saldo supera limite de 6600"
+            echo "Saldo supera limite de 6600";
             }
         else{
-        echo "Monto no permitido"
+        echo "Monto no permitido";
         }
         }
         
