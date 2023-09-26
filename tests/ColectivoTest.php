41a3203d05e0b3553704394570cddc9b34d5d7e7
number1angel <nicolasrossello1@gmail.com>
2023-09-26 16:57:56 -0300

    Error en test colectivo

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -7,8 +7,7 @@ use PHPUnit\Framework\TestCase;
 class ColectivoTest extends TestCase{ 
     public function testGetlinea(){
         $colectivo = new Colectivo();
-        $tarjeta = new Tarjeta(); //necesito la tarjeta ahre
-        $boleto = new Boleto(); //y el boleto tambien jiji
+        $tarjeta = new Tarjeta();
         
         $this->assertTrue($tarjeta->cargarSaldo(150));
         $this->assertTrue($colectivo->pagarCon($tarjeta));
