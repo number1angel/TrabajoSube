27d79b40dee53979884f3e4d5934b98ea8382eb9
number1angel <nicolasrossello1@gmail.com>
2023-09-29 15:52:38 -0300

    tests29.09

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -17,5 +17,15 @@ class TarjetaTest extends TestCase{
 
         $pruebaSaldo = $tarjeta->verSaldo();
         $this->assertEquals(300, $pruebaSaldo);
+
+        $tarjeta->cargarSaldo(4000);
+        $pruebaSaldo = $tarjeta->verSaldo();
+        $this->assertEquals(4300, $pruebaSaldo);
+
+        $this->expectExceptionMessage('Saldo supera limite de 6600.');
+        $tarjeta->cargarSaldo(4000);
+        
+        $pruebaSaldo = $tarjeta->verSaldo();
+        $this->assertEquals(4300, $pruebaSaldo);
     }
 }
