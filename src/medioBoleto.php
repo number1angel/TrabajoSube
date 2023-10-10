39423b3e701f748e4445cbb5ad234191db2df48c
lucaskriegerson <131319464+lucaskriegerson@users.noreply.github.com>
2023-10-10 15:56:27 -0300

    Update medioBoleto.php

--- src/medioBoleto.php
+++ src/medioBoleto.php
@@ -1,6 +1,20 @@
 <?php
 namespace TrabajoSube;
 
+interface TiempoInterface {
+
+    public function time();
+
+}
+
+class Tiempo implements TiempoInterface {
+
+    public function time() {
+        return time();
+    }
+
+}
+    
 class medioBoleto extends Tarjeta{
     public function pagarTarifa($tarifa){
     $this->updateSaldo(-$tarifa/2);
